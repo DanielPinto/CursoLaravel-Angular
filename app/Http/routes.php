0b8dc33d142b13ed4f057bc629f8a77a24ab522b
Daniel Pinto <danielsp.pinto@gmail.com>
2016-03-19 09:40:50 -0300

    foi feito o agrupamento das rotas e aplicado a autenticação pelo token em todas as rotas.

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -27,50 +27,56 @@ Route::post('oauth/access_token', function(){
 Route::get('user' , 'UserController@index');
 Route::get('user/{id}' , 'UserController@show');
 
+//
 
-
-Route::get('client' , 'ClientController@index');
-Route::get('client/{id}' , 'ClientController@show');
-Route::post('client' , 'ClientController@store');
-Route::put('client/{id}' , 'ClientController@update');
-Route::delete('client/{id}' , 'ClientController@destroy');
+Route::group(['middleware'=>'oauth'],function(){
 
 
 
-Route::get('project' , 'ProjectController@index');
-Route::get('project/{id}' , 'ProjectController@show');
-Route::post('project' , 'ProjectController@store');
-Route::put('project/{id}' , 'ProjectController@update');
-Route::delete('project/{id}' , 'ProjectController@destroy');
+    Route::get('client' ,'ClientController@index');
+    Route::get('client/{id}' , 'ClientController@show');
+    Route::post('client' , 'ClientController@store');
+    Route::put('client/{id}' , 'ClientController@update');
+    Route::delete('client/{id}' , 'ClientController@destroy');
 
 
 
-Route::get('project/{id}/tasks' , 'ProjectTaskController@index');
-Route::get('project/{id}/tasks/{taskId}' , 'ProjectTaskController@show');
-Route::post('project/tasks' , 'ProjectTaskController@store');
-Route::put('project/{id}/tasks/{taskId}' , 'ProjectTaskController@update');
-Route::delete('project/{id}/tasks{taskId}' , 'ProjectTaskController@destroy');
+    Route::get('project' , 'ProjectController@index');
+    Route::get('project/{id}' , 'ProjectController@show');
+    Route::post('project' , 'ProjectController@store');
+    Route::put('project/{id}' , 'ProjectController@update');
+    Route::delete('project/{id}' , 'ProjectController@destroy');
 
 
 
-Route::get('project/{id}/note' , 'ProjectNoteController@index');
-Route::get('project/{id}/note/{noteId}' , 'ProjectNoteController@show');
-Route::post('project/note' , 'ProjectNoteController@store');
-Route::put('project/{id}/note/{noteId}' , 'ProjectNoteController@update');
-Route::delete('project/{id}/note/{noteId}' , 'ProjectNoteController@destroy');
+    Route::get('project/{id}/tasks' , 'ProjectTaskController@index');
+    Route::get('project/{id}/tasks/{taskId}' , 'ProjectTaskController@show');
+    Route::post('project/tasks' , 'ProjectTaskController@store');
+    Route::put('project/{id}/tasks/{taskId}' , 'ProjectTaskController@update');
+    Route::delete('project/{id}/tasks{taskId}' , 'ProjectTaskController@destroy');
 
 
 
-
-Route::get('project/{id}/members' , 'ProjectMembersController@index');
-Route::get('project/{id}/members/{memberId}' , 'ProjectMembersController@show');
-Route::post('project/{id}/members' , 'ProjectMembersController@store');
-Route::delete('project/{id}/members/{memberId}' , 'ProjectMembersController@destroy');
+    Route::get('project/{id}/note' , 'ProjectNoteController@index');
+    Route::get('project/{id}/note/{noteId}' , 'ProjectNoteController@show');
+    Route::post('project/note' , 'ProjectNoteController@store');
+    Route::put('project/{id}/note/{noteId}' , 'ProjectNoteController@update');
+    Route::delete('project/{id}/note/{noteId}' , 'ProjectNoteController@destroy');
 
 
 
 
-Route::post('project/{id}/file' , 'ProjectFileController@store');
-Route::get('project/{id}/file' , 'ProjectFileController@index');
-Route::get('project/{id}/file/{idfile}' , 'ProjectFileController@show');
-Route::delete('project/{id}/file/{idfile}' , 'ProjectFileController@destroy');
+    Route::get('project/{id}/members' , 'ProjectMembersController@index');
+    Route::get('project/{id}/members/{memberId}' , 'ProjectMembersController@show');
+    Route::post('project/{id}/members' , 'ProjectMembersController@store');
+    Route::delete('project/{id}/members/{memberId}' , 'ProjectMembersController@destroy');
+
+
+
+
+    Route::post('project/{id}/file' , 'ProjectFileController@store');
+    Route::get('project/{id}/file' , 'ProjectFileController@index');
+    Route::get('project/{id}/file/{idfile}' , 'ProjectFileController@show');
+    Route::delete('project/{id}/file/{idfile}' , 'ProjectFileController@destroy');
+
+});
--- config/oauth2.php
+++ config/oauth2.php
@@ -36,6 +36,15 @@ return [
                 'access_token_ttl' => 3600
             ]
 
+     /*
+        'refresh_token'=>[
+
+            'class'=> '\League\OAuth2\Server\Grant\RefreshTokenGrant',
+            'access-token_ttl' => 3600,
+            'refresh_token_ttl' => 36000
+        ]
+     */
+
     ],
 
     /*
